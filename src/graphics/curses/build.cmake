bf5767371edd9689a65d547b45dda2ea4c225bb2
Claire Clark-Muth <claire.clark.muth@gmail.com>
2022-07-07 11:34:06 -0600

    Initial build system configuration.

---
+++ src/graphics/curses/build.cmake
@@ -0,0 +1,25 @@
+# Attempt to find the name of the system curses library if not specified.
+if( NOT PROJECT_CURSES_LIBRARY )
+	find_library( PROJECT_CURSES_LIBRARY
+	NAMES
+		ncurses
+		pdcurses
+	HINTS
+		${PROJECT_SOURCE_DIR}/lib/${PROJECT_BUILD_TYPE}/${PROJECT_ARCH}/${PROJECT_PLATFORM}
+	NO_CACHE
+	REQUIRED
+	)
+endif()
+
+set_target_properties( ${RENDER_LIBRARY}
+PROPERTIES
+	C_STANDARD 17
+)
+target_sources( ${RENDER_LIBRARY}
+PRIVATE
+	curses/CursesDevice.c
+)
+target_link_libraries( ${RENDER_LIBRARY}
+PUBLIC
+	${CURSES_LIBRARY}
+)
